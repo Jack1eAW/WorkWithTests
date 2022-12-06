d5f475e1847c42e8d151a039256a2bd870ea5e84
Jack1eAW <alcheb2014@gmail.com>
2022-12-06 21:00:08 +0300

    Commit 5: Added all features for test

--- TestFunc/MathFunction.swift
+++ TestFunc/MathFunction.swift
@@ -9,8 +9,33 @@ import Foundation
 
 class Function {
     
-    func ResultOfFunction(x: Float) -> Float {
+    func resultOfFunction(x: Float) -> Float {
         let y = (pow(x, 3) * cos(x) - exp(2*x)).rounded()
+        return y
+    }
+    
+    func powerX(x: Float) -> Float {
+        let y = pow(x, 3)
+        return y
+    }
+    
+    func muliply(a: Float, b: Float) -> Float {
+        let y = a * b
+        return y
+    }
+    
+    func minus(a: Float, b: Float) -> Float {
+        let y = a - b
+        return y
+    }
+    
+    func powerExp(x: Float) -> Float {
+        let y = exp(2 * x)
+        return y
+    }
+    
+    func checkCos(x: Float) -> Float {
+        let y = cos(x)
         return y
     }
 }
--- TestFuncTests/TestFuncTests.swift
+++ TestFuncTests/TestFuncTests.swift
@@ -11,20 +11,75 @@ import XCTest
 final class TestFuncTests: XCTestCase {
 
     let function = Function()
+    let x: Float = 2.0
     
-    func functionResult() {
-        let x: Float = 2.0
+    func testResultOfFunction() {
         let expected: Float = -58.0
-        let result = function.ResultOfFunction(x: x)
+        let result = function.resultOfFunction(x: x)
+        
         XCTAssert(result == expected, "Expected \(expected), got \(result)")
     }
     
-    override func setUpWithError() throws {}
-
-    override func tearDownWithError() throws {}
-
-    func testExample() throws {}
-
-    func testPerformanceExample() throws { self.measure {} }
-
+    func testPowerX() {
+        let expected: Float = 8.0
+        let result = function.powerX(x: x)
+        
+        XCTAssert(result == expected, "Expected \(expected), got \(result)")
+    }
+    
+    func testMultiply() {
+        let a = pow(x, 3)
+        let b = cos(x)
+        let expected = 1
+        let result = function.muliply(a: a, b: b)
+        
+        XCTAssert(Int(result) < expected, "Expected \(expected), got \(result)")
+    }
+    
+    func testMinus() {
+        let a = pow(x, 3) * cos(x)
+        let b = exp(2*x)
+        let expected = 1
+        let result = function.minus(a: a, b: b)
+        
+        XCTAssert(Int(result) < expected, "Expected \(expected), got \(result)")
+    }
+    
+    func testPowerExp() {
+        let expected: Float = 8.0
+        let result = function.powerExp(x: x)
+        
+        XCTAssert(result > expected, "Expected \(expected), got \(result)")
+    }
+    
+    func testCheckCosLess() {
+        let expected: Float = 1.0
+        let result = function.checkCos(x: x)
+        
+        XCTAssert(result < expected, "Expected \(expected), got \(result)")
+    }
+    
+    func testCheckCosMore() {
+        let expected: Float = -1
+        let result = function.checkCos(x: x)
+        
+        XCTAssert(result > expected, "Expected \(expected), got \(result)")
+    }
+    
+    func testCheckCosOnMoreThan() {
+        let x: Float = -10.0
+        let expected: Float = 1
+        let result = function.checkCos(x: x)
+        
+        XCTAssert(result < expected, "Expected \(expected), got \(result)")
+    }
+    
+    func testValue() {
+        let x: Float = 0.0
+        let expected: Float = 0.0
+        let result = function.resultOfFunction(x: x)
+        
+        XCTAssert(result < expected, "Expected \(expected), got \(result)")
+    }
+    
 }
