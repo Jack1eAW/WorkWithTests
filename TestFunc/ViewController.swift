2ed516a871567bc63b164ee476be4fdd412dee9b
Jack1eAW <alcheb2014@gmail.com>
2022-12-06 16:32:28 +0300

    Initial Commit

---
+++ TestFunc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestFunc
+//
+//  Created by Aleksandr Chebotarev on 12/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
